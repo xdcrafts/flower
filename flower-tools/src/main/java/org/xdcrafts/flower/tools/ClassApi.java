9f6cc3353f6a8735ed747cc3e4e1fb0509be2a42
Vadim Dubs <catofflames@gmail.com>
2016-12-27 10:45:12 +0600

    New helper method: resolveActualTypeArgs.

--- flower-tools/src/main/java/org/xdcrafts/flower/tools/ClassApi.java
+++ flower-tools/src/main/java/org/xdcrafts/flower/tools/ClassApi.java
@@ -16,11 +16,20 @@
 
 package org.xdcrafts.flower.tools;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
  * Utility methods for Class.
  */
+@SuppressWarnings("unchecked")
 public final class ClassApi {
 
     private ClassApi() {
@@ -57,6 +66,78 @@ public final class ClassApi {
             result = (T) o;
         }
         return Optional.ofNullable(result);
+    }
+
+    /**
+     * Resolves the actual generic type arguments for a base class, as viewed from a subclass or implementation.
+     *
+     * @param <T> base type
+     * @param offspring class or interface subclassing or extending the base type
+     * @param base base class
+     * @param actualArgs the actual type arguments passed to the offspring class
+     * @return actual generic type arguments, must match the type parameters of the offspring class. If omitted, the
+     * type parameters will be used instead.
+     */
+    public static <T> Type[] resolveActualTypeArgs(Class<? extends T> offspring, Class<T> base, Type... actualArgs) {
+        if (offspring == null
+            || base == null
+            || (actualArgs.length != 0 && actualArgs.length != offspring.getTypeParameters().length)) {
+            throw new IllegalArgumentException(
+                "offspring and base should not be null, number ot actualArgs should match number of offspring types"
+            );
+        }
+        if (actualArgs.length == 0) {
+            actualArgs = offspring.getTypeParameters();
+        }
+        Map<String, Type> typeVariables = new HashMap<>();
+        for (int i = 0; i < actualArgs.length; i++) {
+            TypeVariable<?> typeVariable = offspring.getTypeParameters()[i];
+            typeVariables.put(typeVariable.getName(), actualArgs[i]);
+        }
+        List<Type> ancestors = new LinkedList<>();
+        if (offspring.getGenericSuperclass() != null) {
+            ancestors.add(offspring.getGenericSuperclass());
+        }
+        Collections.addAll(ancestors, offspring.getGenericInterfaces());
+        for (Type type : ancestors) {
+            if (type instanceof Class<?>) {
+                Class<?> ancestorClass = (Class<?>) type;
+                if (base.isAssignableFrom(ancestorClass)) {
+                    Type[] result = resolveActualTypeArgs((Class<? extends T>) ancestorClass, base);
+                    if (result != null) {
+                        return result;
+                    }
+                }
+            }
+            if (type instanceof ParameterizedType) {
+                ParameterizedType parameterizedType = (ParameterizedType) type;
+                Type rawType = parameterizedType.getRawType();
+                if (rawType instanceof Class<?>) {
+                    Class<?> rawTypeClass = (Class<?>) rawType;
+                    if (base.isAssignableFrom(rawTypeClass)) {
+                        List<Type> resolvedTypes = new LinkedList<>();
+                        for (Type t : parameterizedType.getActualTypeArguments()) {
+                            if (t instanceof TypeVariable<?>) {
+                                Type resolvedType = typeVariables.get(((TypeVariable<?>) t).getName());
+                                resolvedTypes.add(resolvedType != null ? resolvedType : t);
+                            } else {
+                                resolvedTypes.add(t);
+                            }
+                        }
+
+                        Type[] result = resolveActualTypeArgs(
+                            (Class<? extends T>) rawTypeClass,
+                            base,
+                            resolvedTypes.toArray(new Type[] {})
+                        );
+                        if (result != null) {
+                            return result;
+                        }
+                    }
+                }
+            }
+        }
+        return offspring.equals(base) ? actualArgs : new Type[]{};
     }
 
     /**
