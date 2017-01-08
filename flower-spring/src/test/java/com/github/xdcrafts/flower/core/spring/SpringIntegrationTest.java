92c0f8a2d311366bf8be5cafbe1e7f8b73fd896e
xdcrafts <catofflames@gmail.com>
2017-01-08 14:42:32 +0600

    Dependency from spring parent pom removed, spring boot dependencies removed

--- flower-spring/src/test/java/com/github/xdcrafts/flower/core/spring/SpringIntegrationTest.java
+++ flower-spring/src/test/java/com/github/xdcrafts/flower/core/spring/SpringIntegrationTest.java
@@ -23,7 +23,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashMap;
@@ -41,7 +41,7 @@ import static org.junit.Assert.assertTrue;
 @SuppressWarnings("OptionalGetWithoutIsPresent")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles({"email-feature", "sms-feature"})
-@SpringApplicationConfiguration(locations = "/application-context.xml")
+@ContextConfiguration(locations = "/application-context.xml")
 public class SpringIntegrationTest {
 
     @Autowired
